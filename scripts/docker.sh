66d249527ccbe1f5cd8808c19309644cb8249e80
Viktor (Icon) VAD <vad.viktor@gmail.com>
2017-09-19 13:41:38 +0100

    Upgrade docker script.

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -7,21 +7,22 @@ sed -i 's/GRUB_CMDLINE_LINUX=""/GRUB_CMDLINE_LINUX="cgroup_enable=memory swapacc
 # install mandatory packages
 aptitude update && \
 aptitude install -y apt-transport-https \
-                    ca-certificates
-apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-echo 'deb https://apt.dockerproject.org/repo ubuntu-xenial main' > /etc/apt/sources.list.d/docker.list
-aptitude update && \
-aptitude purge lxc-docker && \
-aptitude install -y docker-engine \
-                    apparmor
+                    ca-certificates \
+                    curl \
+                    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+apt-key fingerprint 0EBFCD88
 
-# https://docs.docker.com/engine/admin/systemd/#custom-docker-daemon-options
-# enable Google DNS servers, lock in the default bridge ip, open API on unsecured tcp/ip
-mkdir -p /etc/systemd/system/docker.service.d
-echo "[Service] \n\
-ExecStart= \n\
-ExecStart=/usr/bin/docker daemon -H fd:// --bip=172.17.42.1/16 --dns 8.8.8.8 --dns 8.8.4.4 -H tcp://0.0.0.0:4242" > /etc/systemd/system/docker.service.d/daemon.conf
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+aptitude update && \
+aptitude install -y docker-ce
 
 # add docker group and add vagrant to it
 groupadd docker
 usermod -a -G docker vagrant
+
+systemctl enable docker
